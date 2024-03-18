9068461d28347be5bbe923c863d40b720a74d2a5
DAE94 <david.alcaraz@institutvidreres.cat>
2024-03-18 10:00:44 +0100

    Recuperar contrasenya i verificar usuari

--- servidor.js
+++ servidor.js
@@ -3,6 +3,8 @@ const cors=require('cors');
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 var jp = require('jsonpath');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const fs = require('fs');
 const path = require('path');
 
@@ -11,6 +13,14 @@ const path = require('path');
 const serviceAccount = require('./botigaonline-dam-firebase-adminsdk-fkr0g-fda07391af.json');
 const string_decoder = require("string_decoder");
 
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'daniel.gomez@institutvidreres.cat',
+        pass: '11042003.Dgs'
+    }
+});
 
 initializeApp({
     credential: cert(serviceAccount)
@@ -22,6 +32,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 port = 3080;
 
@@ -42,7 +53,7 @@ app.get('/login', async (req, res)=>{
 });
 
 app.post('/registrar', async (req, res) => {
-    const data = {usuari: req.body.usuari, password: req.body.password, nom: req.body.nom, email: req.body.email, telefon: req.body.telefon}
+    const data = {usuari: req.body.usuari, password: req.body.password, nom: req.body.nom, email: req.body.email, telefon: req.body.telefon, verificat: false}
     const doc = await db.collection('USUARIS').doc(req.body.usuari).get();
     if (doc.exists){
         res.json({missatge: "L'usuari ja existeix", mlog: "intent creació usuari ja existent: ", key: `${data.usuari}`})
@@ -76,7 +87,50 @@ app.post('/infoPerfil', async (req,res)=>{
 
 app.post('/logs', (req, res) =>{
     const logMissatge = req.body.missatge;
-    const arxiuLogs = fs.createWriteStream('C:\\Users\\david\\IdeaProjects\\ZapasWapasServer\\logs.txt', {flags: 'a+'});
+    const arxiuLogs = fs.createWriteStream('logs.txt', {flags: 'a+'});
     arxiuLogs.end(logMissatge+'\n')
     res.json({missatge: 'log creat correctament'});
+});
+app.post('/recuperarContrasenya', (req, res) => {
+    const { email } = req.body;
+
+    const mailOptions = {
+        from: 'daniel.gomez@institutvidreres.cat',
+        to: email,
+        subject: 'Canvi de contrasenya',
+        text: 'Aquí pots canviar la teva contrasenya: [link de cambio de contraseña]'
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('Error al enviar al enviar el correu');
+        } else {
+            console.log('Correu enviat: ' + info.response);
+            res.status(200).send('Correu enviat');
+        }
+    });
+});
+
+app.get('/verificarCorreu', async (req, res) => {
+    try {
+        const { email, nomUsuari, enllacVerificacio } = req.query;
+
+        const mailOptions = {
+            from: 'daniel.gomez@institutvidreres.cat',
+            to: email,
+            subject: 'Verificació usuari',
+            html: `<p>Hola ${nomUsuari}, fes click per a verificar l'usuari <a href="${enllacVerificacio}">Verificar usuari</a></p>`
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        await firebase.database().ref('USUARIS').child(nomUsuari).update({ verificat: true });
+
+        res.send('Usuari verificat.');
+
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Error al verificar usuari.');
+    }
 });
